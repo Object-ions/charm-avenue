68da13b0526a75f6839cf6a39425bef71cb7dd3c
Object-ions <moshikoatia@gmail.com>
2024-05-30 19:13:27 -0700

    Add Product item and nest in HomeScreen

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,18 @@
+import { Card } from 'react-bootstrap';
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`} alt={product.name}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`} alt={product.name}>
+          <Card.Title as="div">{product.name}</Card.Title>
+        </a>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
