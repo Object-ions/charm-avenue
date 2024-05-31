ddcf6c5e7f899342d97061d5a8d6d79440cea66e
Object-ions <moshikoatia@gmail.com>
2024-05-30 19:31:31 -0700

    Add Routes to the app

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,16 @@
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`} alt={product.name}>
+      <Link to={`/product/${product._id}`} alt={product.name}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`} alt={product.name}>
+        <Link to={`/product/${product._id}`} alt={product.name}>
           <Card.Title as="div">{product.name}</Card.Title>
-        </a>
+        </Link>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
