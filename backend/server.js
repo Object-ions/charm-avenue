2c17e8f62ac00d437f4e699ee22dd22ee0cde3b6
Object-ions <moshikoatia@gmail.com>
2024-06-02 12:47:33 -0700

    Set up dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import products from './data/products.js';
-const port = 4000;
+
+const port = process.env.PORT;
 
 const app = express();
 
