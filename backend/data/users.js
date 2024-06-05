fc4e42641056815e06e9283663488b52e3e082dd
Object-ions <moshikoatia@gmail.com>
2024-06-04 22:13:32 -0700

    Prep user data to seed

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcryptjs from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@gmail.com',
+    password: bcryptjs.hashSync('123456', 10), //this will hash the password. the 10 represents the level of hashing, is called 'salt' and it is between 1 to 20 (10 is right in he middle)
+    isAdmin: true,
+  },
+  {
+    name: 'Jon Doe',
+    email: 'jon@gmail.com',
+    password: bcryptjs.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@gmail.com',
+    password: bcryptjs.hashSync('123456', 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
