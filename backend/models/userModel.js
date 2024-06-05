27431257d288c7a6e898782928a776a7424eb9b3
Object-ions <moshikoatia@gmail.com>
2024-06-04 22:00:12 -0700

    Set up all data models

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import { timestamp } from 'rxjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
+
+const User = new mongoose.model('User', userSchema);
+
+export default User;
