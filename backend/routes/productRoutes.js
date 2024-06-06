3ac8376aadfbba0e2cc8be8d864bbcdf9874b7b6
Object-ions <moshikoatia@gmail.com>
2024-06-05 19:42:52 -0700

    Add product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); // to get all of them we will pass an empty object
-    res.json(products);
-  })
-);
-
-// Get a specific product by ID
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
