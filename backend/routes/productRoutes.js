7a251ab54a7de326dc96783a75e1add15db90b68
Object-ions <moshikoatia@gmail.com>
2024-06-05 17:52:27 -0700

    Apply asyncHandler to handle promises on productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,21 @@
 import express from 'express';
-import products from '../data/products.js';
-
+import asyncHandler from '../middleware/asyncHandler.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    res.json(products);
+  })
+);
 
-router.get('/:id', (req, res) => {
-  // Finding a specific product
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    // Finding a specific product
+    const product = products.find((product) => product._id === req.params.id);
+    res.json(product);
+  })
+);
 
 export default router;
