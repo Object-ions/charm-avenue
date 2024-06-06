63b51c366cbc178bd70ab4c2e685f4a36032b9f1
Object-ions <moshikoatia@gmail.com>
2024-06-05 18:52:14 -0700

    Fix typo bug

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,28 @@
 import express from 'express';
-import asyncHandler from '../middleware/asyncHandler.js';
 const router = express.Router();
+
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
+    const products = await Product.find({}); // to get all of them we will pass an empty object
     res.json(products);
   })
 );
 
+// Get a specific product by ID
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    // Finding a specific product
-    const product = products.find((product) => product._id === req.params.id);
-    res.json(product);
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      return res.json(product);
+    } else {
+      return res.status(404).json({ message: 'Product not found' });
+    }
   })
 );
 
