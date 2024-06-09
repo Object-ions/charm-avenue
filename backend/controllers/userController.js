934e32800005b873adf94bd23f40258050c6a3be
Object-ions <moshikoatia@gmail.com>
2024-06-08 18:58:16 -0700

    Successfully validate user email in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,20 @@ import User from '../models/userModel.js';
 // @route POST '/api/users/login'
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login user');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc Register user
