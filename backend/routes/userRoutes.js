70e4b6f80f0ab6cb78fa5616995b0244d39e8dba
Object-ions <moshikoatia@gmail.com>
2024-06-08 22:39:59 -0700

    Successfully update authentication workflow (admin and logged in user)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,11 +12,19 @@ import {
   getUserById,
   updateUser,
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getUsers).post(registerUser);
+router.route('/').get(protect, admin, getUsers).post(registerUser);
 router.post('/logout', logoutUser);
 router.post('/login', loginUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
