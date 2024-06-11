28225a16e024716ac11dc5d572185538694b0349
Object-ions <moshikoatia@gmail.com>
2024-06-10 21:37:21 -0700

    Fix typo

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -24,7 +24,7 @@ const PaymentScreen = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(savePaymentMethod(paymentMethod));
-    navigate('/placeorder');
+    navigate('/place-order');
   };
 
   return (
