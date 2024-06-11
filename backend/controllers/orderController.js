40f0088b1916156a2bc77f82581c95b636e8f6db
Object-ions <moshikoatia@gmail.com>
2024-06-11 10:41:40 -0700

    Add fx to updateOrderToPaid and set a paypal route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,7 +69,24 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route PUT /api/orders/:id/pay
 // @access Private/Admin
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('Update order to paid');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = new Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc Update order to delivered
