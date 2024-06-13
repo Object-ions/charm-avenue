3ca7a36a3030f2a387cb0660b8c403621d1c7aed
Object-ions <moshikoatia@gmail.com>
2024-06-13 15:04:21 -0700

    Update syntax

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -48,7 +48,7 @@ const createProduct = asyncHandler(async (req, res) => {
 // @route PUT /api/products/:id
 // @access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
-  const { name, price, image, brand, category, countInStock, description } =
+  const { name, price, description, image, brand, category, countInStock } =
     req.body;
 
   const product = await Product.findById(req.params.id);
@@ -56,14 +56,14 @@ const updateProduct = asyncHandler(async (req, res) => {
   if (product) {
     product.name = name;
     product.price = price;
+    product.description = description;
     product.image = image;
     product.brand = brand;
     product.category = category;
     product.countInStock = countInStock;
-    product.description = description;
 
     const updatedProduct = await product.save();
-    res.status(201).json(updatedProduct);
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error('Product not found');
