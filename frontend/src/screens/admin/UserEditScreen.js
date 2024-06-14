f2e4e98f16cbaee1ea255e306fbeada9cc765e71
Object-ions <moshikoatia@gmail.com>
2024-06-14 01:16:45 -0700

    Admin user can fetch users edit profile

--- frontend/src/screens/admin/UserEditScreen.js
+++ frontend/src/screens/admin/UserEditScreen.js
@@ -1,7 +1,117 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import FormContainer from '../../components/FormContainer';
+import { toast } from 'react-toastify';
+import {
+  useUpdateUserMutation,
+  useGetUserDetailsQuery,
+} from '../../slices/usersApiSlice';
 
 const UserEditScreen = () => {
-  return <div>UserEditScreen</div>;
+  const { id: userId } = useParams();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const {
+    data: user,
+    isLoading,
+    error,
+    refetch,
+  } = useGetUserDetailsQuery(userId);
+
+  const [updateUser, { isLoading: isLoadingUpdate }] = useUpdateUserMutation();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user, isLoading, error]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await updateUser({
+        userId,
+        name,
+        email,
+        isAdmin,
+      }).unwrap(); //here we need to unwrap the Promise to catch any rejection in our catch block
+
+      toast.success('User updated');
+      refetch(); // This ensures that the UI reflects the most up-to-date data from the server.
+      navigate('/admin/user-list');
+    } catch (error) {
+      toast.error(error.data?.message || error.message);
+    }
+  };
+
+  return (
+    <>
+      <Link to={`/admin/user-list`} className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {isLoadingUpdate && <Loader />}
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error?.error}
+          </Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="email" className="my-2">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="isAdmin" className="my-2">
+              <Form.Label>Is Admin</Form.Label>
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button
+              type="submit"
+              variant="primary"
+              style={{ marginTop: '1rem' }}
+            >
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
 };
 
 export default UserEditScreen;
