327cc762fad732fe860e376764e870683e9e47e0
Object-ions <moshikoatia@gmail.com>
2024-06-14 15:52:14 -0700

    Successfully implement pagination for products on HomeScreen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 15;
+  const pageSize = 2;
   const page = Number(req.query.pageNumber) || 1;
   const count = await Product.countDocuments(); // will get the total number of 'Product'
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
   const { pageNumber } = useParams();
@@ -30,6 +31,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
