3f46612e7d20cad6dd35a1778d45a21c2ca9e7c4
Object-ions <moshikoatia@gmail.com>
2024-06-14 17:02:01 -0700

    Successfully implement the search query in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import logo from '../assets/logo.png';
+import SearchBox from './SearchBox';
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -39,6 +40,8 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <SearchBox />
+
               <LinkContainer to="/cart">
                 <Nav.Link>
                   {' '}
---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Form, Button, FormControl } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const SearchBox = () => {
+  const navigate = useNavigate();
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword || '');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} className="d-flex">
+      <FormControl
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        placeholder="Search items"
+        className="mr-sm-2 ml-sm-5"
+      ></FormControl>
+      <Button type="submit" variant="outline-light" className="p-2 mx-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+export default SearchBox;
