49077c7a363e4014ef160867fcae7ec379cba774
Object-ions <moshikoatia@gmail.com>
2024-06-16 15:31:32 -0700

    Update header presentation and fx

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,12 +1,27 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import {
+  Badge,
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Offcanvas,
+  Button,
+} from 'react-bootstrap';
+import {
+  LuShoppingCart,
+  LuUser,
+  LuUserPlus,
+  LuMenu,
+  LuUserCog,
+} from 'react-icons/lu';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useLogoutMutation } from '../../slices/usersApiSlice';
 import { logout } from '../../slices/authSlice';
-// import SearchBox from '../SearchBox';
+import SearchBox from '../SearchBox';
 import { resetCart } from '../../slices/cartSlice';
+import { useState } from 'react';
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -16,6 +31,11 @@ const Header = () => {
   const navigate = useNavigate();
 
   const [logoutApiCall] = useLogoutMutation();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const logoutHandler = async () => {
     try {
@@ -32,19 +52,52 @@ const Header = () => {
     <header>
       <Navbar expand="md" collapseOnSelect>
         <Container>
+          <Button variant="link" onClick={handleShow} className="menu-icon">
+            <LuMenu size={24} />
+          </Button>
+
           <LinkContainer to="/">
-            <Navbar.Brand>Charm Avenue Jewelry</Navbar.Brand>
+            <Navbar.Brand className="mx-auto">
+              Charm Avenue Jewelry
+            </Navbar.Brand>
           </LinkContainer>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              {/* <SearchBox /> */}
+          <Nav>
+            {userInfo ? (
+              <NavDropdown title={<LuUser size={24} />} id="username">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <LuUser size={24} /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
 
+            {userInfo && userInfo.isAdmin && (
+              <NavDropdown title={<LuUserCog size={24} />} id="adminmenu">
+                <LinkContainer to="/admin/product-list">
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/admin/user-list">
+                  <NavDropdown.Item>Users</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/admin/order-list">
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            )}
+
+            {!userInfo.isAdmin && (
               <LinkContainer to="/cart">
                 <Nav.Link>
-                  {' '}
-                  <FaShoppingCart /> Cart{' '}
+                  <LuShoppingCart size={24} />
                   {cartItems.length > 0 && (
                     <Badge pill bg="success" style={{ marginLeft: '5px' }}>
                       {cartItems.reduce((acc, item) => acc + item.qty, 0)}
@@ -52,42 +105,29 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link>
-                    {' '}
-                    <FaUser /> Sign In{' '}
-                  </Nav.Link>
-                </LinkContainer>
-              )}
-
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="/admin/product-list">
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/user-list">
-                    <NavDropdown.Item>User</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/order-list">
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+            )}
+          </Nav>
         </Container>
       </Navbar>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="flex-column">
+            <LinkContainer to="/products">
+              <Nav.Link onClick={handleClose}>Jewelry Collection</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link onClick={handleClose}>About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link onClick={handleClose}>Contact Us</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
     </header>
   );
 };
