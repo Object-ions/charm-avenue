ece4c0ecbc970e70587b55027db3bbb8e2328597
Object-ions <moshikoatia@gmail.com>
2024-06-16 12:25:24 -0700

    Update PromiseComponent

--- frontend/src/components/PromiseComponent.js
+++ frontend/src/components/PromiseComponent.js
@@ -1,5 +1,8 @@
 import { Container } from 'react-bootstrap';
-import img1 from '../assets/images/1.png';
+import asset1 from '../assets/images/asset1.png';
+import asset2 from '../assets/images/asset2.png';
+import asset3 from '../assets/images/asset3.png';
+import asset4 from '../assets/images/asset4.png';
 
 const PromiseComponent = () => {
   return (
@@ -8,28 +11,28 @@ const PromiseComponent = () => {
       <div className="promise-grid">
         <div className="promise-item">
           <div className="image-wrapper">
-            <img src={img1} alt="promise" />
+            <img src={asset1} alt="promise" />
             <h6>Free worldwide shipping & returns</h6>
           </div>
         </div>
 
         <div className="promise-item">
           <div className="image-wrapper">
-            <img src={img1} alt="promise" />
+            <img src={asset2} alt="promise" />
           </div>
           <h6>Ethically sourced and crafted</h6>
         </div>
 
         <div className="promise-item">
           <div className="image-wrapper">
-            <img src={img1} alt="promise" />
+            <img src={asset3} alt="promise" />
           </div>
           <h6>Recycled post-consumer gold</h6>
         </div>
 
         <div className="promise-item">
           <div className="image-wrapper">
-            <img src={img1} alt="promise" />
+            <img src={asset4} alt="promise" />
           </div>
           <h6>Sustainable lab-grown diamonds</h6>
         </div>
