d057c6f3b1d7eaf14268de866b161a8dad380468
Object-ions <moshikoatia@gmail.com>
2024-06-16 14:10:28 -0700

    Update presentation

--- frontend/src/components/Informative.js
+++ frontend/src/components/Informative.js
@@ -1,27 +1,23 @@
 import { Container } from 'react-bootstrap';
-import model1 from '../assets/images/model1.png';
-import model2 from '../assets/images/model2.png';
+import promise1 from '../assets/images/promise1.png';
+import promise2 from '../assets/images/promise2.png';
 
 const Informative = () => {
   return (
     <Container className="Informative">
-      <div className="item-wrapper" style={{ background: `url(${model1})` }}>
+      <div className="item-wrapper" style={{ background: `url(${promise1})` }}>
         <h3> Lorem Ipsum</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nostrum
           est aliquam Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quasi voluptate dolorem natus! Sunt enim dicta quibusdam ut quia
-          fugiat mollitia.
         </p>
       </div>
 
-      <div className="item-wrapper" style={{ background: `url(${model2})` }}>
+      <div className="item-wrapper" style={{ background: `url(${promise2})` }}>
         <h3> Lorem Ipsum</h3>
         <p>
           rem ea sunt hic consequatur distinctio animi non accusamus vitae!
           Eaque, deserunt consequuntur illum exercitationem ut in nisi. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Ullam,
-          necessitatibus?
         </p>
       </div>
     </Container>
