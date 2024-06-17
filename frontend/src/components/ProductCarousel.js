ae7beee54fe040c9ef86eddf14b536d2f534a81c
Object-ions <moshikoatia@gmail.com>
2024-06-17 12:49:40 -0700

    Fix broken link

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,7 +22,7 @@ const ProductCarousel = () => {
           <Carousel pause="hover" className="mb-4">
             {products.map((product) => (
               <Carousel.Item key={product._id}>
-                <Link to={`/product/${product._id}`}>
+                <Link to={`/products/${product._id}`}>
                   <Image src={product.image} alt={product.name} fluid />
                   <Carousel.Caption className="carousel-caption">
                     <h2 className="text-white text-right">
