072c1a4e528ce33ecb528268f39b78ddced215cc
Object-ions <moshikoatia@gmail.com>
2024-06-17 00:10:06 -0700

    Update presentation

--- frontend/src/components/Upcoming.js
+++ frontend/src/components/Upcoming.js
@@ -1,11 +1,16 @@
 import { Container } from 'react-bootstrap';
 import earring from '../assets/images/earring.png';
+import { Link } from 'react-router-dom';
 
 const Upcoming = () => {
   return (
     <Container className="Upcoming" style={{ background: `url(${earring})` }}>
-      <h2>New Collection</h2>
-      <h2>September 2024</h2>
+      <Link to="/products">
+        <h2>New Collection</h2>
+      </Link>
+      <Link to="/products">
+        <h2>Spring 2024</h2>
+      </Link>
     </Container>
   );
 };
--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -5,44 +5,46 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer style={{ background: `url(${footerImage})` }}>
-      <div className="footer-text">
-        <h2>Charm Avenue </h2>
-        <div className="visit-us">
-          <h4>Location</h4>
-          <p>
-            208 W San Francisco St, Ste B
-            <br />
-            Santa Fe, NM 87501
-          </p>
-          <p>(505) 983-9774</p>
-          <p>
-            Sun - Fri : 10 AM - 6 PM
-            <br />
-            Saturday : Closed
-          </p>
+      <div className="footer-main">
+        <div className="footer-text">
+          <h2>Charm Avenue Jewelry</h2>
+          <div className="visit-us">
+            <h4>Location</h4>
+            <p>
+              1612 Main St, Ste B
+              <br />
+              San Francisco, CA 94114
+            </p>
+            <p>(415) 783-9974</p>
+            <p>
+              Sun - Fri : 10 AM - 6 PM
+              <br />
+              Sat - Sun : Closed
+            </p>
+          </div>
+          <div className="footer-policies">
+            <h4>Policies</h4>
+            <Link to={'/privacy-policy'}>Privacy Policy</Link>
+            <Link to={'/shipping-policy'}>Shipping Policy</Link>
+            <Link to={'/return-policy'}>Return Policy</Link>
+            <Link to={'/store-hours'}>Store Hours</Link>
+          </div>
+          <div className="navigation">
+            <h4>Menu</h4>
+            <Link to={'/profile'}>User Profile</Link>
+            <Link to={'/collections'}>Jewelry Collection</Link>
+            <Link to={'/treatments'}>Treatments</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/contact'}>Contact US</Link>
+          </div>
+          <div className="news-letter">
+            <h4>Join Our News Letter!</h4>
+            <label>Email Address:</label>
+            <input type="email" />
+            <button>Subscribe</button>
+          </div>
+          <p className="copy">Charm Avenue Jewelry &copy; {currentYear}</p>
         </div>
-        <div className="footer-policies">
-          <h4>Policies</h4>
-          <Link to={'/privacy-policy'}>Privacy Policy</Link>
-          <Link to={'/shipping-policy'}>Shipping Policy</Link>
-          <Link to={'/return-policy'}>Return Policy</Link>
-          <Link to={'/store-hours'}>Store Hours</Link>
-        </div>
-        <div className="navigation">
-          <h4>Menu</h4>
-          <Link to={'/profile'}>User Profile</Link>
-          <Link to={'/collections'}>Skincare Collection</Link>
-          <Link to={'/technology'}>Technology</Link>
-          <Link to={'/treatments'}>Treatments</Link>
-          <Link to={'/about'}>About</Link>
-        </div>
-        <div className="news-letter">
-          <h4>Join Our News Letter!</h4>
-          <label>Email Address:</label>
-          <input type="email" />
-          <button>Subscribe</button>
-        </div>
-        <p className="copy">Charm Avenue Jewelry &copy; {currentYear}</p>
       </div>
     </footer>
   );
